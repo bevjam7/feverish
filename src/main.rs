034961a3404998c89d3eecd1a6a4075ced460e3c
doomy <codeberg.org.matrimony703@passmail.net>
2026-02-07 14:44:02 -0500

    inital project setup:

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+// Support configuring Bevy lints within code.
+#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
+// Disable console on Windows for non-dev builds.
+#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+
+use bevy::{asset::AssetMetaCheck, prelude::*};
+
+fn main() -> AppExit {
+    App::new().add_plugins(AppPlugin).run()
+}
+
+pub struct AppPlugin;
+
+impl Plugin for AppPlugin {
+    fn build(&self, app: &mut App) {
+        // Add Bevy plugins.
+        app.add_plugins(
+            DefaultPlugins
+                .set(AssetPlugin {
+                    // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                    // This causes errors and even panics on web build on itch.
+                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Window {
+                        title: "Feverish".to_string(),
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }
+                    .into(),
+                    ..default()
+                }),
+        );
+
+        // Order new `AppSystems` variants by adding them here:
+        app.configure_sets(
+            Update,
+            (
+                AppSystems::TickTimers,
+                AppSystems::RecordInput,
+                AppSystems::Update,
+            )
+                .chain(),
+        );
+
+        // Set up states
+        app.init_state::<Paused>()
+            .init_state::<AppState>()
+            .add_sub_state::<GameState>();
+        app.configure_sets(Update, PausableSystems.run_if(in_state(Paused(false))));
+
+        // Spawn the main camera.
+        app.add_systems(Startup, spawn_camera);
+    }
+}
+
+/// High-level groupings of systems for the app in the `Update` schedule.
+/// When adding a new variant, make sure to order it in the `configure_sets`
+/// call above.
+#[derive(SystemSet, Debug, Clone, Copy, Eq, PartialEq, Hash, PartialOrd, Ord)]
+enum AppSystems {
+    /// Tick timers.
+    TickTimers,
+    /// Record player input.
+    RecordInput,
+    /// Do everything else (consider splitting this into further variants).
+    Update,
+}
+
+/// The overarching app lifecycle state.
+#[derive(States, Copy, Clone, Eq, PartialEq, Hash, Debug, Default)]
+enum AppState {
+    #[default]
+    Load,
+    Game,
+}
+
+/// The in-game state
+#[derive(SubStates, Copy, Clone, Eq, PartialEq, Hash, Debug, Default)]
+#[source(AppState = AppState::Game)]
+enum GameState {
+    #[default]
+    Main,
+}
+
+/// A system set for systems that shouldn't run while the game is paused.
+#[derive(SystemSet, Copy, Clone, Eq, PartialEq, Hash, Debug)]
+struct PausableSystems;
+
+/// Whether or not the game is paused.
+#[derive(States, Copy, Clone, Eq, PartialEq, Hash, Debug, Default)]
+struct Paused(pub bool);
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn((Name::new("Camera"), Camera2d));
+}
